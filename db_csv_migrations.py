d071cfd546f6b7d4cb993d72ae9962ef8423d77f
Stephen Chin <ikmujn780@gmail.com>
2019-08-14 14:26:09 -0400

    Added Real Book tunes in B, real book page numbers, reworked some styles and tempos

--- db_csv_migrations.py
+++ db_csv_migrations.py
@@ -1,33 +1,51 @@
 import csv
 import os
 
-from musicsessions.models import Tune, Style, Tempo
+from musicsessions.models import Tune, Style, Tempo, TuneRealBook, RealBook
 
+# To run file, open shell using:
+# /> py manage.py shell
+# and run the code:
+# >>> import db_csv_migrations
+# >>> db_csv_migrations.main()
 
-def import_song_list(path):
+def main():
+    """This function runs the core of the program."""
+    import_song_list('exports.csv')
+
+def import_song_list(filename : str) -> None:
     """
     Imports the list of songs defined in a csv file given in path.
     """
     print("\n\n\n\n")
+    print("Running song list import on " + filename + ":\n")
     BASE_DIR = os.getcwd()
     import_count = 0
     failed_count = 0
-    with open(path) as f:
+    with open(filename) as f:
         reader = csv.reader(f)
         reader.__next__()
         for row in reader:
-            _, created = Tune.objects.get_or_create(
+            _, tune_created = Tune.objects.get_or_create(
                 name=row[0],
                 composer=row[1],
                 default_style=Style.objects.get(name=row[2]),
                 default_tempo=Tempo.objects.get(name=row[3]),
             )
-            if (not created):
+            _, tunerealbook_created = TuneRealBook.objects.get_or_create(
+                real_book=RealBook.objects.get(name=row[4]),
+                tune=Tune.objects.get(name=row[0]),
+                page_num=row[5]
+            )
+            message = ""
+            if (not tune_created):
                 failed_count += 1
-                print (row[0] + " is already in the database.")
+                message = "Failed: " + row[0] + " is already in the database." + ("Real Book entry added." if tunerealbook_created else "")
+                if (not tunerealbook_created):
+                    message = message + 
             else:
                 import_count += 1
+                message = "Passed: " + row[0] + "."
+            print(message)
     print("\n\n")
     print("Sucessfully imported: " + str(import_count) + ", Failed: " + str(failed_count))
-
-import_song_list('exports.csv')
